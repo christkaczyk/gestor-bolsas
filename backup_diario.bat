51ba165601feae3faa5abc51bb62659a4346ef65
Chris Tkaczyk <tkaczyky@gmail.com>
2026-02-28 14:46:07 -0300

    se agregaron los .bat

---
+++ backup_diario.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM === CONFIGURACION ===
+set DB_NAME=gestor_bolsas
+set DB_USER=postgres
+set BACKUP_PATH=Z:\gestor-bolsas\backups
+set PGPASSWORD=admin123
+
+REM === FECHA Y HORA ===
+for /f %%i in ('powershell -command "Get-Date -Format yyyy_MM_dd_HH_mm"') do set FECHA=%%i
+
+REM === BACKUP ===
+"C:\Program Files\PostgreSQL\18\bin\pg_dump.exe" -U %DB_USER% %DB_NAME% > "%BACKUP_PATH%\gestor_bolsas_backup_%FECHA%.sql"
+
+echo Backup completado!
+pause
